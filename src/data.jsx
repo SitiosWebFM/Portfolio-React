453a8dcbfcf7e5091bd955fa4b8ce55dff26c9d1
SitiosWebFM <sitioswebfm01@gmail.com>
2022-12-01 14:30:08 -0300

    Se agrega proyecto al portfolio

--- src/data.jsx
+++ src/data.jsx
@@ -2,17 +2,17 @@ export const dataProjects= [
   {
     id:0,
     name:"hypecity",
-    img:"img/dispositivos-hypecity.png",
-    logo:"img/logo-hypecity.jpeg",
-    gif:"img/gif-ecommerce.gif",
+    img:"img/screen-hypecity/dispositivos-hypecity.png",
+    logo:"img/screen-hypecity/logo-hypecity.jpeg",
+    gif:"img/screen-hypecity/gif-ecommerce.gif",
     link:"https://ecommercehypecity.netlify.app/",
     description:'Este es el proyecto final del trabajo de React para la "Carrera de Desarrollo Full Stack". Se llevó a cabo hacer un E-commerce, en el cual me base en una marca de ropa Argentina que me gusta mucho. Las tecnologías o librerías que se usaron fueron: React(principal), Fireabse, Router, Bootstrap, Sweet Alert, Emailjs.',
   },
   {
     id:1,
     name:"bybian",
-    img:"img/dispositivos-bybian.png",
-    logo:"img/logo-bybian.PNG",
+    img:"img/screen-bybian/dispositivos-bybian.png",
+    logo:"img/screen-bybian/logo-bybian.PNG",
     sections:[
       "img/screen-bybian/ilustracionesbybian.netlify.app_index.html.png",
       "img/screen-bybian/ilustracionesbybian.netlify.app_pag_nosotros.html.png",
@@ -26,11 +26,11 @@ export const dataProjects= [
   {
     id:2,
     name:"auri",
-    img:"img/dispositivos-auri.png",
-    logo:"img/logo-auri.png",
+    img:"img/screen-auri/dispositivos-auri.png",
+    logo:"img/screen-auri/logo-auri.png",
     sections:[
-      "img/screen-bybian/www.transportesauri.com.png",
-      "img/screen-bybian/www.transportesauri.com_nosotros.html.png",
+      "img/screen-auri/www.transportesauri.com.png",
+      "img/screen-auri/www.transportesauri.com_nosotros.html.png",
   
     ],
     link:"https://www.transportesauri.com/",
@@ -40,10 +40,10 @@ export const dataProjects= [
   {
     id:3,
     name:"sitioswebfm",
-    img:"img/dispositivos-fm.png",
+    img:"img/screen-fm/dispositivos-fm.png",
     logo:"https://sitioswebfm.netlify.app/img/logofm.png",
     sections:[
-      "img/screen-bybian/sitioswebfm.netlify.app_.png"
+      "img/screen-fm/sitioswebfm.netlify.app_.png"
     ],
     link:"https://sitioswebfm.netlify.app/",
     description:'Página web desarrollada para ofrecer mis servicios como Freelancer. Las tecnologías o librerías que se usaron fueron: Html, Css, Js(jQuey).',
@@ -53,9 +53,9 @@ export const dataProjects= [
     id:4,
     project:'test',
     name:"apihorario",
-    img:"img/dispositivos-horario.png",
-    logo:"img/logo-horario.png",
-    gif:"img/gif-horario.gif",
+    img:"img/screen-horario/dispositivos-horario.png",
+    logo:"img/screen-horario/logo-horario.png",
+    gif:"img/screen-horario/gif-horario.gif",
     link:"https://fmhorarioglobal.netlify.app/",
     description:'Test que realicé para formarme y conocer más sobre las apis. Utilicé la API de Abstract API, para saber el horario de cualquier lugar del mundo.',
 
@@ -64,11 +64,21 @@ export const dataProjects= [
     id:5,
     project:'test',
     name:"apiclima",
-    img:"img/dispositivos-clima.png",
-    logo:"img/logo-clima.png",
-    gif:"img/gif-clima.gif",
+    img:"img/screen-clima/dispositivos-clima.png",
+    logo:"img/screen-clima/logo-clima.png",
+    gif:"img/screen-clima/gif-clima.gif",
     link:"https://fmclima.netlify.app/",
     description:'Este es uno de los test que hice para practicar e informarme sobre las apis (Application Programming Interfaces). Utilicé la API de OpenWeatherMap, para crear una aplicación del clima y saber cómo esta el día en cualquier parte del mundo.',
+  },
+  {
+    id:6,
+    project:'singup-app',
+    name:"singup",
+    img:"img/screen-singup/dispositivos-singup.jpg",
+    logo:"img/screen-singup/logo-singup.png",
+    gif:"img/screen-singup/gif-singup.gif",
+    link:"https://singup-app.netlify.app/",
+    description:'Esta es una Aplicación web en la cual se utiliza como librería principal React para el desarrollo completo de la web. El elemento secundario es Firebase, que se utilizó para la autentificación de los usuarios y la base de datos.',
   }
  
 ]
